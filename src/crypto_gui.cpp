eae549bd72d5825a8fa730d70a940bf29c2a0523
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2021-12-12 20:49:38 +0100

    Responsive Gui Template

--- src/crypto_gui.cpp
+++ src/crypto_gui.cpp
@@ -11,12 +11,12 @@ enum
 };
 
 wxBEGIN_EVENT_TABLE(CryptoGui, wxFrame)
-EVT_MENU(ID_Hello, CryptoGui::OnHello)
-EVT_MENU(wxID_EXIT, CryptoGui::OnExit)
-EVT_MENU(wxID_ABOUT, CryptoGui::OnAbout)
-wxEND_EVENT_TABLE()
+    EVT_MENU(ID_Hello, CryptoGui::OnHello)
+        EVT_MENU(wxID_EXIT, CryptoGui::OnExit)
+            EVT_MENU(wxID_ABOUT, CryptoGui::OnAbout)
+                wxEND_EVENT_TABLE()
 
-wxIMPLEMENT_APP(CryptoBot);
+                    wxIMPLEMENT_APP(CryptoBot);
 
 bool CryptoBot::OnInit()
 {
@@ -47,82 +47,86 @@ CryptoGui::CryptoGui(const wxString &title, const wxPoint &pos, const wxSize &si
 
     CreateStatusBar();
     SetStatusText("Welcome to Crypto Gui!");
-  
-    // Create a new Panel //
+
+    // // Create a new Panel //
     wxPanel *panel = new wxPanel(this, -1);
     bool isFromUser = false;
     CryptoGuiPanel *item = new CryptoGuiPanel(panel, isFromUser);
-
 }
 
-CryptoGuiPanel::CryptoGuiPanel(wxPanel *parent, bool isFromUser)
-    : wxPanel(parent, -1, wxPoint(-1, -1), wxSize(800, 450), wxBORDER_NONE) {
-    
-    // Define Panels
-    wxPanel *leftPanel = new wxPanel(this, -1, wxPoint(-1, -1), wxSize(250, 450), wxBORDER_NONE);
-    wxPanel *rightPanel = new wxPanel(this, -1, wxPoint(250, 0), wxSize(550, 450), wxBORDER_NONE);
-    
-    // Define Box Sizers
-    wxBoxSizer *leftvbox = new wxBoxSizer(wxVERTICAL);
-    wxBoxSizer *rightvbox = new wxBoxSizer(wxVERTICAL);
+// CryptoGuiPanel::CryptoGuiPanel(wxPanel *parent, bool isFromUser)
+//     : wxPanel(parent, -1, wxPoint(-1, -1), wxSize(800, 450), wxBORDER_NONE) {
 
+CryptoGuiPanel::CryptoGuiPanel(wxPanel *parent, bool isFromUser)
+{
+
+    //wxPanel *panelLeft1 = new wxPanel(this, -1);
+
+    // Create Box Sizers
+    wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *vbox1 = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer *vbox2 = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer *hleftbox1 = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *hleftbox2 = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *hleftbox3 = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *hleftbox4 = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *hrighttbox1 = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *hrighttbox2 = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *vrighttbox1 = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer *vrighttbox2 = new wxBoxSizer(wxVERTICAL);
 
-    wxBoxSizer *hrightbox1 = new wxBoxSizer(wxHORIZONTAL);
-    wxBoxSizer *hrightbox2 = new wxBoxSizer(wxHORIZONTAL);
 
     // Define elements
-    wxButton *createData = new wxButton(leftPanel, -1, wxT("Create Data"));
-    wxButton *simulateStrategy = new wxButton(leftPanel, -1, wxT("Simulate strategy"));
-    wxButton *cryptoBot = new wxButton(leftPanel, -1, wxT("Crypto Bot"));
-    wxButton *stop = new wxButton(leftPanel, -1, wxT("Stop"));
+    wxButton *createData = new wxButton(parent, -1, wxT("Create Data"));
+    wxButton *simulateStrategy = new wxButton(parent, -1, wxT("Simulate strategy"));
+    wxButton *cryptoBot = new wxButton(parent, -1, wxT("Crypto Bot"));
+    wxButton *stop = new wxButton(parent, -1, wxT("Stop"));
 
-    wxStaticText * staticData = new wxStaticText(rightPanel, -1, "Static Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
-    wxStaticText * statisticsData = new wxStaticText(rightPanel, wxID_ANY, "Statistics Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
-
+    wxStaticText * staticData = new wxStaticText(parent, -1, "Static Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
+    wxStaticText * statisticsData = new wxStaticText(parent, wxID_ANY, "Statistics Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
+    wxStaticText * graphics = new wxStaticText(parent, wxID_ANY, "Graphics", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
+    
     // wxBoxSizer *hbox1 = new wxBoxSizer(wxHORIZONTAL);
     // wxCheckBox *cb1 = new wxCheckBox(panel, wxID_ANY, wxT("BitCoint"));
     // wxChoice *ch1 = new wxChoice(panel, wxID_ANY, wxPoint(10,10), wxSize(100,40), 3, &str1, wxCB_SORT | wxCB_DROPDOWN);
-    
+
     // const wxString str1 = wxT("Linux");
     // wxString str2 = wxT("Operating");
     // wxString str3 = wxT("System");
-    
-    // Add elements to Box Sizers
-    
-    // vbox->Add(hbox2, 0, wxALIGN_RIGHT | wxRIGHT | wxBOTTOM, 10);
-    leftvbox->Add(hleftbox1, 2, wxEXPAND);
-    leftvbox->Add(hleftbox2, 2, wxEXPAND);
-    leftvbox->Add(hleftbox3, 2, wxEXPAND);
-    leftvbox->Add(hleftbox4, 1, wxEXPAND);
 
-    rightvbox->Add(hrightbox1, 1, wxEXPAND);
-    rightvbox->Add(hrightbox2, 2, wxEXPAND);
+    // Assign elements
+    hrighttbox1->Add(vrighttbox1, 1, wxEXPAND);
+    hrighttbox1->Add(vrighttbox2, 1, wxEXPAND);
+
+    vbox2->Add(hrighttbox1, 1, wxEXPAND);
+    vbox2->Add(hrighttbox2, 2, wxEXPAND);
+
+    vbox1->Add(hleftbox1, 2, wxEXPAND);
+    vbox1->Add(hleftbox2, 2, wxEXPAND);
+    vbox1->Add(hleftbox3, 2, wxEXPAND);
+    vbox1->Add(hleftbox4, 1, wxEXPAND);
+
+    hbox->Add(vbox1, 1, wxEXPAND);
+    hbox->Add(vbox2, 2, wxEXPAND);
 
     hleftbox1->Add(createData,1);
     hleftbox2->Add(simulateStrategy,1);
     hleftbox3->Add(cryptoBot,1);
     hleftbox4->Add(stop,1);
+   
+    vrighttbox1->Add(staticData,1);
+    vrighttbox2->Add(statisticsData,1);
 
-    hrightbox1->Add(staticData, 1);
-    hrightbox2->Add(statisticsData, 1);
-    
+    hrighttbox2->Add(graphics,1);
+
+
     // Stablish size from Panels
-    leftPanel->SetSizer(leftvbox);
-    rightPanel->SetSizer(rightvbox);
+    parent->SetSizer(hbox);
 
-    // Stablish Background Color to Panels.
-    leftPanel->SetBackgroundColour(wxT("YELLOW"));
-    rightPanel->SetBackgroundColour(wxT("RED"));
-
-    this->SetBackgroundColour((isFromUser == true ? wxT("YELLOW") : wxT("BLUE")));
+    // Stablish Background Color to Panel.
+    //this->SetBackgroundColour((isFromUser == true ? wxT("YELLOW") : wxT("BLUE")));
     
 }
-
-
 
 void CryptoGui::OnExit(wxCommandEvent &event)
 {
@@ -152,5 +156,3 @@ void CryptoGui::OnHello(wxCommandEvent &event)
     _cryptoLogic = std::make_unique<CryptoLogic>();
     _cryptoLogic->startSimulation();
 }
-
-
