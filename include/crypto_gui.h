89a88cbde67880616b11c086b0b8d349f1e283d1
Pablo Ruiz <pablo.ruiz.molina@gmail.com>
2021-12-18 21:31:45 +0100

    Gui Part 2

--- include/crypto_gui.h
+++ include/crypto_gui.h
@@ -3,43 +3,49 @@
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+#include <wx/wx.h>
 #endif
 
 #include <future>
 #include "crypto_logic.h"
 
-class CryptoBot: public wxApp
+class CryptoBot : public wxApp
 {
 public:
-    virtual bool OnInit();
+  virtual bool OnInit();
 };
 
-
-class CryptoGui: public wxFrame
+class CryptoGui : public wxFrame
 {
 public:
-    CryptoGui(const wxString& title, const wxPoint& pos, const wxSize& size);
+  CryptoGui(const wxString &title, const wxPoint &pos, const wxSize &size);
+
 private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-    wxDECLARE_EVENT_TABLE();
+  void OnHello(wxCommandEvent &event);
+  void OnExit(wxCommandEvent &event);
+  void OnAbout(wxCommandEvent &event);
+  wxDECLARE_EVENT_TABLE();
 
-    std::unique_ptr<CryptoLogic> _cryptoLogic;
-    
+  std::unique_ptr<CryptoLogic> _cryptoLogic;
 };
 
-
-class CryptoGuiPanel : public wxPanel {
+class CryptoGuiPanel : public wxPanel
+{
 private:
   // control elements
   //wxStaticBitmap *_chatBotImg;
-  
+
   wxStaticText *strategy_type;
   wxStaticText *exchange;
   wxStaticText *commission_value;
   wxStaticText *cryptocurrency_type;
+
+  wxStaticText *position_bool;
+  wxStaticText *last_order_cc_value;
+  wxStaticText *last_order_$_value;
+  wxStaticText *benefits_value;
+  wxStaticText *benefits_acc_value;
+  wxStaticText *interest_value;
 
 public:
   // constructor / destructor
--- src/crypto_gui.cpp
+++ src/crypto_gui.cpp
@@ -11,12 +11,12 @@ enum
 };
 
 wxBEGIN_EVENT_TABLE(CryptoGui, wxFrame)
-    EVT_MENU(ID_Hello, CryptoGui::OnHello)
-        EVT_MENU(wxID_EXIT, CryptoGui::OnExit)
-            EVT_MENU(wxID_ABOUT, CryptoGui::OnAbout)
-                wxEND_EVENT_TABLE()
+EVT_MENU(ID_Hello, CryptoGui::OnHello)
+EVT_MENU(wxID_EXIT, CryptoGui::OnExit)
+EVT_MENU(wxID_ABOUT, CryptoGui::OnAbout)
+wxEND_EVENT_TABLE()
 
-                    wxIMPLEMENT_APP(CryptoBot);
+wxIMPLEMENT_APP(CryptoBot);
 
 bool CryptoBot::OnInit()
 {
@@ -60,8 +60,6 @@ CryptoGui::CryptoGui(const wxString &title, const wxPoint &pos, const wxSize &si
 
 CryptoGuiPanel::CryptoGuiPanel(wxPanel *parent, bool isFromUser)
 {
-
-
     //wxPanel *panelLeft1 = new wxPanel(this, -1);
 
     // Create Box Sizers
@@ -69,7 +67,7 @@ CryptoGuiPanel::CryptoGuiPanel(wxPanel *parent, bool isFromUser)
     wxBoxSizer *vbox1 = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer *vbox2 = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer *hleftbox1 = new wxBoxSizer(wxHORIZONTAL);
-    wxBoxSizer *hleftbox2 = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *hleftbox2 = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer *hleftbox3 = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *hleftbox4 = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *hrighttbox1 = new wxBoxSizer(wxHORIZONTAL);
@@ -77,36 +75,69 @@ CryptoGuiPanel::CryptoGuiPanel(wxPanel *parent, bool isFromUser)
     wxBoxSizer *vrighttbox1 = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer *vrighttbox2 = new wxBoxSizer(wxVERTICAL);
 
+    wxBoxSizer *simulate_label_box = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *simulate_choice_box = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *simulate_buttons_box = new wxBoxSizer(wxHORIZONTAL);
+
     wxBoxSizer *strategy_box = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *exchange_box = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *commission_box = new wxBoxSizer(wxHORIZONTAL);
     wxBoxSizer *cryptocurrency_box = new wxBoxSizer(wxHORIZONTAL);
 
-
+    wxBoxSizer *position_box = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *last_order_cc_box = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *last_order_$_box = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *benefits_box = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *benefits_acc_box = new wxBoxSizer(wxHORIZONTAL);
+    wxBoxSizer *interest_box = new wxBoxSizer(wxHORIZONTAL);
 
     // Define elements
     wxButton *createData = new wxButton(parent, -1, wxT("Create Data"));
-    wxButton *simulateStrategy = new wxButton(parent, -1, wxT("Simulate strategy"));
+    //wxButton *simulateStrategy = new wxButton(parent, -1, wxT("Simulate strategy"));
     wxButton *cryptoBot = new wxButton(parent, -1, wxT("Crypto Bot"));
     wxButton *stop = new wxButton(parent, -1, wxT("Stop"));
 
-    wxStaticText * staticData = new wxStaticText(parent, -1, "Static Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
-    wxStaticText * statisticsData = new wxStaticText(parent, wxID_ANY, "Statistics Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
-    wxStaticText * graphics = new wxStaticText(parent, wxID_ANY, "Graphics", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
+    wxButton *simulate_btn = new wxButton(parent, -1, wxT("Simulate"));
+    wxButton *historic_btn = new wxButton(parent, -1, wxT("Historic Data"));
+    wxButton *realdata_btn = new wxButton(parent, -1, wxT("Real Data"));
+
+    //wxStaticText * staticData = new wxStaticText(parent, -1, "Static Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
+    //wxStaticText * statisticsData = new wxStaticText(parent, wxID_ANY, "Statistics Data", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_DOUBLE);
+    //wxStaticText * graphics = new wxStaticText(parent, wxID_ANY, "Graphics", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE | wxBORDER_NONE);
+    wxStaticText *graphics = new wxStaticText(parent, -1, wxT("Graphics: "));
+
+    // wxStaticText *strategy = new wxStaticText(parent, wxID_ANY, "Strategy: ", wxPoint(-1, -1), wxSize(90, 30), wxALIGN_CENTRE);
+    wxStaticText *simulate_label = new wxStaticText(parent, -1, wxT("Simulate your strategy here: "));
+
+    const wxString str1 = wxT("Linux");
+    wxString str2 = wxT("Operating");
+    wxString str3 = wxT("System");
+    wxChoice *simulate_choice = new wxChoice(parent, -1, wxPoint(10,10), wxSize(100,40), 3, &str1, wxCB_SORT | wxCB_DROPDOWN);
 
     wxStaticText *strategy = new wxStaticText(parent, -1, wxT("Strategy: "));
     wxStaticText *data_from = new wxStaticText(parent, -1, wxT("Data from: "));
     wxStaticText *commission = new wxStaticText(parent, -1, wxT("Commission: "));
     wxStaticText *cryptocurrency = new wxStaticText(parent, -1, wxT("Cryptocurrency: "));
 
-    // Set text variables
-    // strategy_type = new wxStaticText(parent, -1, wxT(""), wxPoint(10, 10));
-    // strategy_type->SetLabel(wxT("Simple"));
+    wxStaticText *position = new wxStaticText(parent, -1, wxT("Position: "));
+    wxStaticText *last_order_cc = new wxStaticText(parent, -1, wxT("Last order (cc): "));
+    wxStaticText *last_order_$ = new wxStaticText(parent, -1, wxT("Last order ($): "));
+    wxStaticText *benefits = new wxStaticText(parent, -1, wxT("Benefits in last order ($): "));
+    wxStaticText *benefits_acc = new wxStaticText(parent, -1, wxT("Benefits accumulated ($): "));
+    wxStaticText *interest = new wxStaticText(parent, -1, wxT("Interest (%): "));
+
+    // Set text variables (TO DO)
     strategy_type = new wxStaticText(parent, -1, wxT("Simple"));
     exchange = new wxStaticText(parent, -1, wxT("Binance"));
     commission_value = new wxStaticText(parent, -1, wxT("0.000075"));
     cryptocurrency_type = new wxStaticText(parent, -1, wxT("Bitcoint"));
 
+    position_bool = new wxStaticText(parent, -1, wxT("Open"));
+    last_order_cc_value = new wxStaticText(parent, -1, wxT("0.004"));
+    last_order_$_value = new wxStaticText(parent, -1, wxT("192.48"));
+    benefits_value = new wxStaticText(parent, -1, wxT("0.35"));
+    benefits_acc_value = new wxStaticText(parent, -1, wxT("7.17"));
+    interest_value = new wxStaticText(parent, -1, wxT("2.47"));
 
     //const wxString strategy_type = wxT("Simple: ");
     
@@ -118,47 +149,87 @@ CryptoGuiPanel::CryptoGuiPanel(wxPanel *parent, bool isFromUser)
     // wxString str2 = wxT("Operating");
     // wxString str3 = wxT("System");
 
+
     // Assign elements
-    hrighttbox1->Add(vrighttbox1, 1, wxEXPAND);
-    hrighttbox1->Add(vrighttbox2, 1, wxEXPAND);
+    hbox->Add(vbox1, 1, wxEXPAND);
+    hbox->Add(vbox2, 2, wxEXPAND);
 
-    vbox2->Add(hrighttbox1, 1, wxEXPAND);
-    vbox2->Add(hrighttbox2, 2, wxEXPAND);
-
+    //inside hbox
     vbox1->Add(hleftbox1, 2, wxEXPAND);
     vbox1->Add(hleftbox2, 2, wxEXPAND);
     vbox1->Add(hleftbox3, 2, wxEXPAND);
     vbox1->Add(hleftbox4, 1, wxEXPAND);
 
-    hbox->Add(vbox1, 1, wxEXPAND);
-    hbox->Add(vbox2, 2, wxEXPAND);
+    vbox2->Add(hrighttbox1, 1, wxEXPAND);
+    vbox2->Add(hrighttbox2, 2, wxEXPAND);
 
+    //inside vbox1
     hleftbox1->Add(createData,1);
-    hleftbox2->Add(simulateStrategy,1);
+
+    //inside hleftbox2
+    hleftbox2->Add(simulate_label_box, 1, wxEXPAND);
+    hleftbox2->Add(simulate_choice_box, 1, wxEXPAND);
+    hleftbox2->Add(simulate_buttons_box, 1, wxEXPAND);
+
     hleftbox3->Add(cryptoBot,1);
+
     hleftbox4->Add(stop,1);
-   
-    // vrighttbox1->Add(createData);
-    // vrighttbox1->Add(simulateStrategy,0, wxLEFT, 10);
 
-    vrighttbox1->Add(strategy_box,1);
-    vrighttbox1->Add(exchange_box,1);
-    vrighttbox1->Add(commission_box,1);
-    vrighttbox1->Add(cryptocurrency_box,2);
-
-    strategy_box->Add(strategy, 1);
-    strategy_box->Add(strategy_type,1);
-    exchange_box->Add(data_from, 1);
-    exchange_box->Add(exchange,1);
-    commission_box->Add(commission, 1);
-    commission_box->Add(commission_value,1);
-    cryptocurrency_box->Add(cryptocurrency, 1);
-    cryptocurrency_box->Add(cryptocurrency_type,1);
-
-    vrighttbox2->Add(statisticsData,1);
+    //inside vbox2
+    hrighttbox1->Add(vrighttbox1, 1,  wxTOP, 20);
+    hrighttbox1->Add(vrighttbox2, 1,  wxTOP, 20);
 
     hrighttbox2->Add(graphics,1);
 
+
+
+    //inside simulate_label_box
+    simulate_label_box->Add(simulate_label, 1, wxEXPAND);
+    simulate_choice_box->Add(simulate_choice, 1, wxEXPAND);
+    simulate_buttons_box->Add(simulate_btn, 1, wxEXPAND);
+    simulate_buttons_box->Add(historic_btn, 1, wxEXPAND);
+    simulate_buttons_box->Add(realdata_btn, 1, wxEXPAND);
+
+    //inside hrighttbox1
+    vrighttbox1->Add(strategy_box, 1, wxEXPAND);
+    vrighttbox1->Add(exchange_box, 1, wxEXPAND);
+    vrighttbox1->Add(commission_box, 1, wxEXPAND);
+    vrighttbox1->Add(cryptocurrency_box, 1, wxEXPAND);
+    
+    vrighttbox2->Add(position_box, 1, wxEXPAND);
+    vrighttbox2->Add(last_order_cc_box, 1, wxEXPAND);
+    vrighttbox2->Add(last_order_$_box, 1, wxEXPAND);
+    vrighttbox2->Add(benefits_box, 1, wxEXPAND);
+    vrighttbox2->Add(benefits_acc_box, 1, wxEXPAND);
+    vrighttbox2->Add(interest_box, 1, wxEXPAND);
+    
+
+    //inside vrighttbox1
+    strategy_box->Add(strategy, 1, wxALIGN_LEFT | wxLEFT, 20);
+    strategy_box->Add(strategy_type,1, wxALIGN_LEFT);
+    // strategy->SetBackgroundColour(wxT("BLUE"));
+    strategy_type->SetBackgroundColour(wxT("RED"));
+    exchange_box->Add(data_from, 1, wxALIGN_LEFT | wxLEFT, 20);
+    exchange_box->Add(exchange,1, wxALIGN_LEFT);
+    commission_box->Add(commission, 1, wxALIGN_LEFT | wxLEFT, 20);
+    commission_box->Add(commission_value,1, wxALIGN_LEFT);
+    cryptocurrency_box->Add(cryptocurrency, 1, wxALIGN_LEFT | wxLEFT, 20);
+    cryptocurrency_box->Add(cryptocurrency_type,1, wxALIGN_LEFT);
+
+    //inside vrighttbox2
+    position_box->Add(position, 1, wxALIGN_LEFT | wxLEFT, 20);
+    position_box->Add(position_bool,1, wxALIGN_LEFT);
+    last_order_cc_box->Add(last_order_cc, 1, wxALIGN_LEFT | wxLEFT, 20);
+    last_order_cc_box->Add(last_order_cc_value,1, wxALIGN_LEFT);
+    last_order_$_box->Add(last_order_$, 1, wxALIGN_LEFT | wxLEFT, 20);
+    last_order_$_box->Add(last_order_$_value,1, wxALIGN_LEFT);
+    benefits_box->Add(benefits, 1, wxALIGN_LEFT | wxLEFT, 20);
+    benefits_box->Add(benefits_value,1, wxALIGN_LEFT);
+    benefits_value->SetBackgroundColour(wxT("RED"));
+    benefits_acc_box->Add(benefits_acc, 1, wxALIGN_LEFT | wxLEFT, 20);
+    benefits_acc_box->Add(benefits_acc_value,1, wxALIGN_LEFT);
+    interest_box->Add(interest, 1, wxALIGN_LEFT | wxLEFT, 20);
+    interest_box->Add(interest_value,1, wxALIGN_LEFT);
 
     // Stablish size from Panels
     parent->SetSizer(hbox);
